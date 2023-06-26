146a61752374f8f012a496e975d5faf91a868f17
Shobhit Byadwal <92286261+Shobhit444@users.noreply.github.com>
2023-06-26 17:34:53 +0530

    creating desktop client side components

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,66 @@
+import bcrypt from "bcrypt"
+import NextAuth, { AuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
+import prisma from "@/app/libs/prismadb"
+//  exporting a constant named authOptions of type AuthOptions.
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    }),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Invalid credentials');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        });
+
+        if (!user || !user?.hashedPassword) {// is there is no user of there is no hashed password
+          throw new Error('Invalid credentials');
+        }
+//  these above checks are for users who have registered using google or github
+//because they have not provided exclusive passwords to us as they have used google 
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error('Invalid credentials');
+        }
+//cheking if the passowrd is correct or not
+        //if correct retun user
+        return user;
+      }
+    })
+  ],
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+}
+
+const handler = NextAuth(authOptions);
+// purpose of the NextAuth function is to create the authentication handler for your Next.js application. It sets up the necessary routes, middleware, and callbacks to handle authentication-related requests.
+export { handler as GET, handler as POST };
